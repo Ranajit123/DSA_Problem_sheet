b5c76100c6ff4850efe1e9f973eeea6bfdb00b26
Ranajit Das <das143ranajit@gmail.com>
2023-08-31 00:14:04 +0530

    4th commit

--- max_min_element_in_arr.java
+++ max_min_element_in_arr.java
@@ -1,4 +1,9 @@
+// Find the max and min element of a array
+
+import java.util.*;
 public class max_min_element_in_arr{
+
+  // using creting function
   public static int max_in_arr(int arr[]){
     int max = Integer.MIN_VALUE;
     for(int i=0; i<arr.length; i++){
@@ -8,8 +13,24 @@ public class max_min_element_in_arr{
     }
     return max;
   }
+
+  public static int min_in_arr(int arr[]){
+    int min = Integer.MAX_VALUE;
+    for(int i=0; i<arr.length; i++){
+      if(arr[i]<min){
+        min = arr[i];
+      }
+    }
+    return min;
+  }
   public static void main(String[] args) {
     int arr[] = {3, 5, 4, 1, 9};
-    System.out.println("Maximum in the array: "+max_in_arr(arr));
+    // System.out.println("Maximum in the array: "+max_in_arr(arr));
+    // System.out.println("Minimum in the array: "+min_in_arr(arr));
+    
+    // using sorting algorithms
+    Arrays.sort(arr);
+    System.out.println("Maximum in the array: "+arr[arr.length-1]);
+    System.out.println("Minimum in the array: "+arr[0]);
   }
 }
